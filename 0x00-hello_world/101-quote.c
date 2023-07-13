f225e987809d3faa32289ccd15d956d068cf2bd0
MoiponeKhoadi <moiponekhoadi@gmail.com>
2023-07-13 12:30:23 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<unistd.h>
 /**
- * main -  C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ * main -  C program that prints exactly and that piece of art is useful
  * Return: 1 Successful
  */
 int main(void)
