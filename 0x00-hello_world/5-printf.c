37ecddd2436e69b995f57ea0344424f12fdb8a84
MoiponeKhoadi <moiponekhoadi@gmail.com>
2023-07-13 09:55:24 -0700

    C program that print a line followed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  main -  C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
+ *  Return : 0 (Successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
