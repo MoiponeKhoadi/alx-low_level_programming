bc8e52a9d67cc2ea29901ac7a4e156426acc6cc6
MoiponeKhoadi <moiponekhoadi@gmail.com>
2023-07-13 12:08:41 -0700

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main -  C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ * Return: 1 Successful
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\", 59);
+	return(1);
+}
